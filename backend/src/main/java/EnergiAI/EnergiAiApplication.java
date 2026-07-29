ae7b139b2742fe01f2d357b270e1cf4622072e5e
floriceligonz <floriruiz77@gmail.com>
2026-07-28 23:37:33 -0600

    Creacion de estructura  backend EnergiAI

---
+++ backend/src/main/java/EnergiAI/EnergiAiApplication.java
@@ -0,0 +1,13 @@
+package EnergiAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnergiAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnergiAiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/EnergiAI/EnergiAiApplicationTests.java
@@ -0,0 +1,13 @@
+package EnergiAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnergiAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
